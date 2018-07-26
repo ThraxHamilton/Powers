e20225458bd84f5a40b20dc5fe4e735bbe241d2b
ThraxHamilton <mmcclenton16@gmail.com>
2018-07-26 14:04:11 -0500

    .

--- powers.js
+++ powers.js
@@ -2,7 +2,7 @@ console.log('Lord knows....')
 
 // document.querySelector("#activate-flight").addEventListener('click', handlerFunction)
 
-function handlerFunction () {
+function handlerFunction (enableFlight) {
     document.querySelector("#flight").addEventListener('click', function(){
         toggleClass('enabled')
     })
